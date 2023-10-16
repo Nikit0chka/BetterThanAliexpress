2f1f7310306e2bb69cebcb06a6ef1ebeb1aa64b8
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-17 00:28:02 +0400

    Added UserRegistrationController

---
+++ BetterThanAliexpress/Controllers/UserRegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BetterThanAliexpress.Models;
+
+namespace BetterThanAliexpress.Controllers;
+
+public class UserRegistrationController : Controller
+{
+    private readonly ILogger<UserRegistrationController> _logger;
+
+    public UserRegistrationController(ILogger<UserRegistrationController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult SDf()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
