3697981fbf04cae4574e2d87f71e086a56baa132
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-17 00:29:41 +0400

    Added a DataBaseModel. DbModel for ef

---
+++ BetterThanAliexpress/EntityFramework/DataBaseModel.cs
@@ -0,0 +1,41 @@
+﻿namespace BetterThanAliexpress.EntityFramework;
+
+public class Buyer
+{
+    public int Id { get; set; }
+    public required string Login { get; set; }
+    public required string Password { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required DateTime DateOfBirthday { get; set; }
+    public required string Email { get; set; }
+    public ICollection<Product>? ProductBasket { get; set; }
+}
+
+public class Seller
+{
+    public int Id { get; set; }
+    public required string Login { get; set; }
+    public required string Password { get; set; }
+    public required string Name { get; set; }
+    public required int Inn { get; set; }
+    public required string Email { get; set; }
+    public required string CompanyAddress { get; set; }
+    public ICollection<Product>? Products { get; set; }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required Seller Seller { get; set; }
+    public required int Price { get; set; }
+    public required ProductCategory ProductCategory { get; set; }
+}
+
+public class ProductCategory
+{
+    public int Id { get; set; }
+    public required string Name;
+    public ICollection<Product>? Products;
+}
