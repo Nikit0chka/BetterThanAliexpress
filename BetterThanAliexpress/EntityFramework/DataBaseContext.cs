2c34e84c1a9a7710386bbca3adeedb1200425765
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-17 00:30:03 +0400

    Added a DataBaseContext DbContext for ef

---
+++ BetterThanAliexpress/EntityFramework/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace BetterThanAliexpress.EntityFramework;
+
+public class DataBaseContext : DbContext
+{
+    public DbSet<Buyer>? Buyers;
+    public DbSet<Seller>? Sellers;
+    public DbSet<Product>? Products;
+    public DbSet<ProductCategory>? Categories;
+    
+    public DataBaseContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+    optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BetterAliexpressDb;Trusted_Connection=True;");
+}
